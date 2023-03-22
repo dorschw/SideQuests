96947b21a13501ea179f1481ac4cd39518c71f78
dorschw <81086590+dorschw@users.noreply.github.com>
2023-03-22 14:40:17 +0200

    use logging

--- batch_ignore.py
+++ batch_ignore.py
@@ -1,7 +1,10 @@
-from configparser import ConfigParser
-from pathlib import Path
-from functools import lru_cache
+import logging
 import re
+from configparser import ConfigParser
+from functools import lru_cache
+from pathlib import Path
+
+logging.basicConfig(level=logging.INFO)
 
 CONTENT_PATH = Path("../content")
 
@@ -28,30 +31,30 @@ def add_error(pack_name: str, file_name: str, error_code: str) -> None:
             "ignore" in config[file_section_key]
             and error_code in config[file_section_key]["ignore"]
         ):
-            print(f"{error_code} is already ignored in {file_name}, skipping")
+            logging.info(f"{error_code} is already ignored in {file_name}, skipping")
             return
 
         config[file_section_key]["ignore"] += f",{error_code}"
-        print(f"appended {error_code} to the ignore list of {file_name}")
+        logging.info(f"appended {error_code} to the ignore list of {file_name}")
         
     else:
         config.add_section(file_section_key)
         config.set(file_section_key, "ignore", error_code)
-        print(f"added a new ignore section with {error_code} to {file_name}")
+        logging.info(f"added a new ignore section with {error_code} to {file_name}")
 
     with pack_ignore_path.open("w") as f:
         config.write(f, space_around_delimiters=False)
 
 def auto_ignore_all(error_string:str):
-    error_regex = re.compile("Packs\/(?P<pack_name>.*)\/.*\/(?P<file>.*\..*) - \[(?P<error_code>[A-Z]{2}\d{3})\]")
+    error_regex = re.compile(r".*Packs\/(?P<pack_name>.*)\/.*\/(?P<file>.*\..*) - \[(?P<error_code>[A-Z]{2}\d{3})\]")
     for line in filter(None, error_string.splitlines()):
         if match := error_regex.match(line):
             add_error(pack_name=match['pack_name'], file_name=match['file'], error_code=match['error_code'])
         else:
-            print(f"could not parse {line=}")
+            logging.error(f"could not parse {line=}")
 
-# EXAMPLE RUN 
+# # EXAMPLE RUN 
 # auto_ignore_all("""
-# Packs/DeprecatedContent/Scripts/script-DefaultIncidentClassifier.yml - [BA120],
-# Packs/trendMicroDsm/Scripts/script-TrendMicroClassifier_README.md - [BA120]
+# myPacks/DeprecatedContent/Scripts/script-DefaultIncidentClassifier.yml - [BA120],
+# Packs/trendMicroDsm/Scripts/script-TrendMicroClassifier_README.md - [BA120]asdf
 # """)
